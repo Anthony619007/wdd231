c12c746c9a98d5caa3ad07a693c5972a90219935
Anthony619007 <canusiemnzenwa@byupathway.edu>
2026-08-13 17:06:33 +0000

    updateeeeee

--- scripts/join.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const timestampField = document.getElementById("form-timestamp");
-    if (timestampField) {
-        // Automatically injects current date and time string on load
-        timestampField.value = new Date().toISOString();
-    }
-});
